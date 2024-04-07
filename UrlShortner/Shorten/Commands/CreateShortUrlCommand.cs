b6fca9eac22e11d984fceda7628801bf4bfb55bd
kapros <17946039+kapros@users.noreply.github.com>
2024-04-07 22:09:34 +0200

    add command & handler for creating short urls

---
+++ UrlShortner/Shorten/Commands/CreateShortUrlCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortner.Shorten.Commands;
+
+public class CreateShortUrlCommand(string url, HttpRequest httpRequest) : ICommand
+{
+    public string Url => url;
+    public HttpRequest HttpRequest => httpRequest;
+}
---
+++ UrlShortner/Shorten/ShortUrlCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using UrlShortner.Shorten.Commands;
+
+namespace UrlShortner.Shorten;
+
+public class ShortUrlCommandHandler(IUrlShorteningService urlShorteningService) : ICommandHandler<CreateShortUrlCommand, string>
+{
+    // also thinking here that maybe the shortening itself could / should be broken down to generating the ID and the DB insert
+    // in which case, technically, the command here is simplified to have no return but also be more atomic
+    // on the other hand, it risks introducing unnecessary complexity in the calling handler
+    public async Task<string> Handle(CreateShortUrlCommand input)
+    {
+        var result = await urlShorteningService.ShortenUrl(input.Url, input.HttpRequest);
+        return result.Short;
+    }
+}
