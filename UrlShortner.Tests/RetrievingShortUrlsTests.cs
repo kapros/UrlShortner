b56bfef581fd45a2a6063ed21537c98151959c6b
kapros <17946039+kapros@users.noreply.github.com>
2024-04-09 12:31:10 +0200

    add tests for getting urls

---
+++ UrlShortner.Tests/RetrievingShortUrlsTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Dynamic;
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using UrlShortner.DataAccess;
+
+namespace UrlShortner.Tests;
+public class RetrievingShortUrlsTests
+    : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _client;
+    private readonly char[] _code = "a1Bc12".ToCharArray();
+    public RetrievingShortUrlsTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        using var scope = _factory.Server.Services.CreateScope();
+        var scopedServices = scope.ServiceProvider;
+        var dbContext = scopedServices.GetRequiredService<UrlShortnerDbContext>();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+        dbContext.ShortenedUrls.Add(new ShortUrl
+        {
+            Code = Code.Create(_code),
+            CreatedOnUtc = DateTime.UtcNow,
+            Short = "https://localhost/" + new string(_code),
+            Long = "https://test.com"
+        });
+        dbContext.SaveChanges();
+        _client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false,
+        });
+    }
+
+    [Fact]
+    public async Task GetFetchesTheUrl()
+    {
+        var response = await _client.GetAsync(new string(_code));
+        Assert.Equal(expected: HttpStatusCode.Found, response.StatusCode);
+        var redirect = response.Headers.Location;
+        Assert.Equal("https://test.com/", redirect.ToString());
+        //Assert.Contains("localhost", shortUrl);
+    }
+
+    [Fact]
+    public async Task GetCachesTheResultIfNotPresent()
+    {
+        var response = await _client.GetAsync(new string(_code));
+        var memCache = _factory.Server.Services.GetRequiredService(typeof(IMemoryCache)) as IMemoryCache;
+        Assert.NotNull(memCache);
+        var cached = memCache.Get(_code);
+        Assert.NotNull(cached);
+    }
+
+}
