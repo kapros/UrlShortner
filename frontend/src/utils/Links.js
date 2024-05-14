2e12f62b457541b6d25d257c7c6eb2c8cee81323
kapros <17946039+kapros@users.noreply.github.com>
2024-05-14 11:35:48 +0200

    remove host url

--- frontend/src/utils/Links.js
+++ frontend/src/utils/Links.js
@@ -1,5 +1,3 @@
-export const hostUrl = "https://localhost:7194/api/v1/";
-
 export const newLinkUrl = "api/v1/shorten";
 export const allLinksUrl = "api/v1/all";
 export const deleteLinkUrl = "api/v1/";
